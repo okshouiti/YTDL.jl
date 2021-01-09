bcf7b78568919508fee20a2550483201fc4e81b2
okshouiti <59249869+okshouiti@users.noreply.github.com>
2021-01-10 01:32:24 +0900

    Refresh all

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # YTDL
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://okshouiti.github.io/YTDL.jl/stable)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://okshouiti.github.io/YTDL.jl/dev)
-[![Build Status](https://travis-ci.com/okshouiti/YTDL.jl.svg?branch=master)](https://travis-ci.com/okshouiti/YTDL.jl)
-[![Codecov](https://codecov.io/gh/okshouiti/YTDL.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/okshouiti/YTDL.jl)
-[![Coveralls](https://coveralls.io/repos/github/okshouiti/YTDL.jl/badge.svg?branch=master)](https://coveralls.io/github/okshouiti/YTDL.jl?branch=master)
+`youtube-dl`や`annie`のようなダウンローダをjuliaで書いてみたかったマン
--- src/YTDL.jl
+++ src/YTDL.jl
@@ -1,27 +1,89 @@
 module YTDL
 
-using HTTP, Gumbo, Cascadia
+using HTTP, LazyJSON, PrettyTables, DataFrames
+export ytdl, available_codecs
 
-export ytdl
 
-function ytdl(url::String)
-    dom = Gumbo.parsehtml(String(HTTP.get(url).body))
-    title = nodeText(Cascadia.eachmatch(sel".watch-title", dom.root)[1])[6:end-5]
-    #thumbnail(url, title)
-    codeclist = "251/140/webm/bestaudio"
-    ytdlCmd = `youtube-dl -f $codeclist $url -o -`
-    ffmpegCmd = `ffmpeg -i pipe:0 -codec copy "H:/download/$title.opus"`
-    run(pipeline(ytdlCmd, ffmpegCmd));
-    println("$title.opus has been saved to H:/download/")
+
+abstract type AV end
+
+struct Audio <: AV
+	url::String
+	length::Int64
+    ext::String
+    temp_ext::String
+    function Audio(nt, ext)
+        # Int32 reach its ceiling at 2^31-1 (≒ 256 MiB)
+        # Int64 accepts up to 2^63-1 (≒ 1 EiB)
+        len = haskey(nt, :length) ? parse(Int64, nt.length) : 0
+        new(nt.url, len, ext.a, ext.atemp)
+    end
 end
 
-# サムネイル取得は試験段階
-function thumbnail(url::String, title::String)
-    id = url[[findlast("?v=", url)...][end]+1:end]
-    thumburl = ["https://i.ytimg.com/vi$fmt/$id/maxresdefault.$ext" for (fmt,ext) in [("", "jpg"), ("_webp", "webp")]]
-    dst = ["H:/download/$title.$ext" for ext in ["jpg", "webp"]]
-    download.(thumburl, dst)
-    println("Thumbnail (jpg & webp) has been saved to H:/download/")
+struct Video <: AV
+	url::String
+	length::Int64
+    ext::String
+    temp_ext::String
+    function Video(nt, ext)
+        len = haskey(nt, :length) ? parse(Int64, nt.length) : 0
+        new(nt.url, len, ext.v, ext.vtemp)
+    end
+    Video(t::Nothing, ext) = nothing
 end
+
+
+
+include("codec.jl")
+include("deps.jl")
+include("meta.jl")
+include("downloader.jl")
+include("presets.jl")
+include("utils.jl")
+include("youtube-dl.jl")
+
+
+
+# global setting
+function ytdl(url::AbstractString; kwargs...)
+    d = get(ENV, "YTDL_DIR", homedir())
+    #opts = haskey(kwargs, :dir) ? kwargs.data : merge((;dir=d), kwargs)
+    opts = get(kwargs, :dir, merge((;dir=d), kwargs))
+    isdir(opts.dir) || return @error("$(opts.dir) does not exist.")
+    HTTP.setuseragent!(nothing)
+    v_id = queries(url)["v"]
+    ytdl_body(v_id, opts)
+    return nothing
+end
+
+# download setting each video
+function ytdl_body(v_id, opts)
+    info = get_metadata(v_id)
+    if isnothing(info)
+        if get(opts, :enable_backend, false)
+            return youtube_dl(v_id, opts)
+        else
+            println("""
+                This video type (cipherd video source URL) is not yet available,
+                but you can download it using youtube-dl backend-mode.
+                Add option `enable_backend=true` and call `ytdl` again!
+            """)
+            return nothing
+        end
+    end
+    path = joinpath(opts.dir, safename(info.title, opts))
+    @async get_thumbnail(v_id, path)
+    c = sellect_format(info.codecs, opts)
+    ext = sellect_ext(c.a, c.v)
+	downloader(
+		Audio(info.codecs[c.a], ext),
+        Video(get(info.codecs, c.v, nothing), ext),
+        path,
+        info2cmd(info, v_id, opts),
+        opts
+    )
+end
+
+
 
 end # module
