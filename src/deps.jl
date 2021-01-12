93551f8150917cd675e646680b9900f294b7e919
okshouiti <59249869+okshouiti@users.noreply.github.com>
2021-01-13 01:07:33 +0900

    Add print dependency func

--- src/deps.jl
+++ src/deps.jl
@@ -1,32 +1,32 @@
-#=function deps()
-    yt = try
-        `youtube-dl --version` |> readlines |> first |> VersionNumber
-    catch
-        @error "youtube-dl can't be found."
-    end
-    ff = try
-        `ffmpeg -version` |> readlines |> first |> x->split(x," ",limit=4)[3] |> VersionNumber
-    catch
-        @error "ffmpeg can't be found."
-    end
-    @info("Dependencies' version",
-        ytdl = yt,
-        ffmpeg = ff
-    )
-    return nothing
-end=#
-
-
-
 abstract type YTDLDeps end
 struct YTDLbin <: YTDLDeps end
 struct FFMPEGbin <: YTDLDeps end
 
 
 
-# 外部バイナリが利用できるか
-isexcutable(cmd) = try success(cmd); catch; false; end
+# test availability of COMMANDS (copied from Franklin.jl)
+isexcutable(cmd)::Bool = try success(cmd); catch; false; end
 
 isavailable(::Type{YTDLbin}) = isexcutable(`youtube-dl --help`)
 
 isavailable(::Type{FFMPEGbin}) = isexcutable(`ffmpeg -h`)
+
+
+
+function deps()
+    yt = try
+        `youtube-dl --version` |> readlines |> first
+    catch
+        "unavailable"
+    end
+    ff = try
+        `ffmpeg -version` |> readlines |> first |> x->split(x," ",limit=4)[3]
+    catch
+        "unavailable"
+    end
+    @info("Dependencies' version",
+        ytdl = yt,
+        ffmpeg = ff
+    )
+    return nothing
+end
